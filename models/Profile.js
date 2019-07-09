4d0fa60e7af173542aae43afcf5d4241d47c8961
Your Name <nazik_111@mail.ru>
2019-07-09 23:03:49 +0600

    add Profile model

---
+++ models/Profile.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+  occupation: { type: String },
+  location: { type: String },
+  country: { type: String },
+  age: { type: Number },
+  bio: { type: String },
+  dialogueApp: [
+    {
+      speaker: {
+        type: Number,
+        required: true
+      },
+      firstLanguage: {
+        type: String,
+        required: true
+      },
+      targetLanguage: {
+        type: String,
+        required: true
+      },
+      mainText: {
+        type: String,
+        required: true
+      },
+      promptText: {
+        type: String,
+        required: true
+      },
+      voiceAudio: { mime: String, bin: Buffer },
+      firstLanguageTip: {
+        type: String
+      },
+      targetLanguageTip: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    linkedIn: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
